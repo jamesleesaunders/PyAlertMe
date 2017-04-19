49ccf6c10e49d715dff61853e3eadafbbc2c7a66
James Saunders <james@saunders-family.net>
2017-04-19 20:37:36 +0100

    Bump to v0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PyAlertMe',
-      version='0.1.4',
+      version='0.2.1',
       description='Python AlertMe',
       url='https://github.com/jamesleesaunders/PyAlertMe',
       author='James Saunders',
