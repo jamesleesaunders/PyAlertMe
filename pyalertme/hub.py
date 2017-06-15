a6de98091be88237925b1e7399071515bf564ad2
James Saunders <jamesleesaunders@gmail.com>
2017-06-15 23:59:14 +0100

    Test

--- pyalertme/hub.py
+++ pyalertme/hub.py
@@ -140,7 +140,7 @@ class Hub(Base):
         else:
             # If not generate new node_id and add to list of known devices.
             node_id = Base.pretty_mac(addr_long)
-            node_obj = Base()
+            node_obj = Device()
             self.nodes[node_id] = {'AddressLong': addr_long, 'Attributes': {}, 'Obj': node_obj}
 
         return node_id
